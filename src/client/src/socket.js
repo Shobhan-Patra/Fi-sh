77b81031d1ababee9194c773d7d114e1e256e8cd
Shobhan-Patra <230170146055@vgecg.ac.in>
2025-10-21 18:50:10 +0530

    Fix socket backend URL

--- src/client/src/socket.js
+++ src/client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // backend URL
-const URL = 'http://localhost:8000';
+const URL = import.meta.env.VITE_API_BASE_URL;
 
 export const socket = io(URL, {
   autoConnect: false,
